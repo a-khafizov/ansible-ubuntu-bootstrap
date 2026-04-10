81f18388bbfd4ba3d842370f76d7b95da2a941e5
khvam <khv.am@ya.ru>
2026-04-10 18:44:25 +0300

    refactoring

--- scripts/bash/setup_git.sh
+++ scripts/bash/setup_git.sh
@@ -3,7 +3,6 @@
 sudo apt update
 sudo apt install -y git
 
-echo "Настройка Git..."
 read -p "Введите ваше имя для Git: " git_name
 read -p "Введите ваш email для Git: " git_email
 
@@ -13,7 +12,3 @@ git config --global init.defaultBranch main
 git config --global pull.rebase false
 git config --global color.ui auto
 git config --global core.editor "code --wait"
-
-echo "Текущая конфигурация:"
-echo "Имя: $(git config --global user.name)"
-echo "Email: $(git config --global user.email)"
